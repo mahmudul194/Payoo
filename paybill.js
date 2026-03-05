3fd48e770fd49f59decc5dcd727fdcf8780d577b
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-06 01:38:21 +0600

    added pay bill logic

---
+++ paybill.js
@@ -0,0 +1,37 @@
+const payBtn = document.getElementById('pay-bill-btn')
+
+payBtn.addEventListener('click',()=>{
+    const paybillselect = document.getElementById('pay-bill-select');
+    const select = paybillselect.value;
+    const paybillbiller = document.getElementById('pay-bill-biller');
+    const biller = paybillbiller.value;
+    const paybillpin = document.getElementById('pay-bill-pin');
+    const pin = paybillpin.value;
+    const paybillamount = document.getElementById('pay-bill-amount');
+    const amount = Number(paybillamount.value);
+    const balance = document.getElementById('balance')
+    const currentbalance = Number(balance.innerText)
+    const remainingbalance = currentbalance - amount;
+
+    if(select === 'Select To Pay'){
+        alert('select a bill to pay');
+        return;
+    }
+    if(biller.length != 11){
+        alert('enter the correct numbere.')
+    }
+    if(remainingbalance < 0){
+        alert('invalid balance stats.')
+        return;
+    }
+    if(pin != '9762'){
+        alert('invaid pin')
+        return;
+    }
+    else{
+        if(select !== 'Select To Pay' && biller.length === 11 && pin === '9762'){
+            alert('payment successful')
+            balance.innerText = remainingbalance;
+        }
+    }
+})
