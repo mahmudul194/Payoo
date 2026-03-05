9572621b44e4a7a16d589757338327fce8037328
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-06 01:38:04 +0600

    added section hiding class

--- machine.js
+++ machine.js
@@ -16,3 +16,22 @@ function setBalance(value){
     const balanceElement = document.getElementById('balance')
     balanceElement.innerText = value;
 }
+
+function ShowOnly(id){
+    const addmoney = document.getElementById('add-money')
+    const cashout = document.getElementById('cashout')
+    const sendmoney = document.getElementById('send-money')
+    const getbonus = document.getElementById('get-bonus')
+    const paybill = document.getElementById('pay-bill')
+    // const transactions = document.getElementById('transactions')
+
+    addmoney.classList.add('hidden')
+    cashout.classList.add('hidden')
+    sendmoney.classList.add('hidden')
+    getbonus.classList.add('hidden')
+    paybill.classList.add('hidden')
+    // transactions.classList.add('hidden')
+
+    const selected =  document.getElementById(id);
+    selected.classList.remove('hidden')
+}
