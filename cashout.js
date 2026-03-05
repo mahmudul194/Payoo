91029fd37a6bd1196de681c195de24dc0e1ad8d3
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-05 13:04:35 +0600

    modified the logic of cashout

--- cashout.js
+++ cashout.js
@@ -1,29 +1,62 @@
-const cashoutBtn = document.getElementById("cashout-btn");
-const agentNumber = document.getElementById("cashout-agent");
-const pinNumber = document.getElementById("cashout-pin");
-const amountNumber = document.getElementById("cashout-amount");
-const balance = document.getElementById("balance");
-
-cashoutBtn.addEventListener("click", () => {
+// used a machine for repetative codes
+const cashoutBtn = document.getElementById('cashout-btn')
+cashoutBtn.addEventListener('click',()=>{
+  const agentNumber = getvaluefromInput('cashout-agent');
+  const pinNumber = getvaluefromInput('cashout-pin');
+  const amount = getvaluefromInput('cashout-amount')
+  const CurrentAmount = Number(amount);
+  const balance = document.getElementById('balance')
   const currentBalance = Number(balance.innerText);
-  const agent = agentNumber.value;
-  const pin = pinNumber.value;
-  const amount = Number(amountNumber.value);
-  const newBalance = currentBalance - amount;
+  const NewBalance = currentBalance - CurrentAmount;
 
-  if (newBalance < 0) {
-    alert("invalid amount");
+  if(NewBalance < 0){
+    alert('invalid amount')
     return;
   }
-  if (agent.length == 11 && pin == "9762") {
-    if (amount >= 100 && amount <= 50000) {
-      alert("withdrawal successful");
-      balance.innerText = `${newBalance}`;
+  if(agentNumber.length === 11 && pinNumber === '9762'){
+    if(CurrentAmount >= 100 && CurrentAmount <= 50000){
+      alert('Cashout successful')
+      balance.innerText = NewBalance;
     }
-  } else {
-    alert("wrong pin or agent Number not valid");
   }
-  agentNumber.value = "";
-  pinNumber.value = "";
-  amountNumber.value = "";
-});
+  else{
+    alert('invalid pin or agent number.')
+  }
+})
+
+
+
+
+
+
+
+// applied normal bigginer like approch.
+// const cashoutBtn = document.getElementById("cashout-btn");
+// const agentNumber = document.getElementById("cashout-agent");
+// const pinNumber = document.getElementById("cashout-pin");
+// const amountNumber = document.getElementById("cashout-amount");
+// const balance = document.getElementById("balance");
+
+// cashoutBtn.addEventListener("click", () => {
+//   const currentBalance = Number(balance.innerText);
+//   const agent = agentNumber.value;
+//   const pin = pinNumber.value;
+//   const amount = Number(amountNumber.value);
+//   const newBalance = currentBalance - amount;
+
+//   if (newBalance < 0) {
+//     alert("invalid amount");
+//     return;
+//   }
+//   if (agent.length == 11 && pin == "9762") {
+//     if (amount >= 100 && amount <= 50000) {
+//       alert("withdrawal successful");
+//       balance.innerText = `${newBalance}`;
+//     }
+//   } else {
+//     alert("wrong pin or agent Number not valid");
+//   }
+//   agentNumber.value = "";
+//   pinNumber.value = "";
+//   amountNumber.value = "";
+// });
