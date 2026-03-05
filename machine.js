4437793dc3015cc8e2b25c50a2e08a954f06862b
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-05 14:09:31 +0600

    added anothe machine

--- machine.js
+++ machine.js
@@ -1,5 +1,18 @@
 function getvaluefromInput(id){
     const input = document.getElementById(id);
     const value = input.value;
+    input.value = ''
     return value;
+}
+
+//
+function getBalance(){
+    const balanceElement = document.getElementById('balance')
+    const balance = balanceElement.innerText;
+    return Number(balance);
+}
+
+function setBalance(value){
+    const balanceElement = document.getElementById('balance')
+    balanceElement.innerText = value;
 }
